14b3a9f602ccfebbba92786a2dfb61db50a28cc7
erik eriksson <error.errorsson@gmail.com>
2017-03-30 11:36:04 +0200

    bridge networking

--- home-assistant/run.sh
+++ home-assistant/run.sh
@@ -4,7 +4,10 @@ CONFIG=$HOME/.home-assistant.docker
 
 mkdir -p $CONFIG
 
-docker run -d --name home-assistant --net=host \
-       -v /etc/localtime:/etc/localtime:ro \
-       -v $CONFIG \
-       molobrakos/home-assistant:latest
+docker run \
+-d \
+--name home-assistant \
+-p 8123:8123/tcp \
+-v /etc/localtime:/etc/localtime:ro \
+-v $CONFIG \
+molobrakos/home-assistant:latest
