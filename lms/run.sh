86e0126bf090dee7cac9c23ac37694c42684c97a
Erik Eriksson <molobrakos@users.noreply.github.com>
2017-03-30 07:51:57 +0200

    fix

--- lms/run.sh
+++ lms/run.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 DIR=$HOME/.lms
+
 CONFIG_DIR=$DIR/config
 CACHE_DIR=$DIR/cache
 LOG_DIR=$DIR/logs
+MUSIC_DIR=$DIR/music
 
 mkdir -p $CONFIG_DIR
 mkdir -p $CACHE_DIR
 mkdir -p $LOG_DIR
+mkdir -p $MUSIC_DIR
 
 docker run -d --name lms \
        -p 9000:9000 \
        -p 3483:3483 \
        -p 3483:3483/udp \
        -v /etc/localtime:/etc/localtime:ro \
-       -v /config:$CONFIG_DIR \
-       -v /cache:$CACHE_DIR \
-       -v /logs:$LOG_DIR \
+       -v $CONFIG_DIR:/config \
+       -v $CACHE_DIR:/cache \
+       -v $LOG_DIR:/logs \
+       -v $MUSIC_DIR:/music \
        molobrakos/lms:latest
 
