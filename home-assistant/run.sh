2cde63890121f243b5bfcd3fc7ee4dcf83c99c72
erik eriksson <error.errorsson@gmail.com>
2017-06-19 12:18:23 +0200

    ok

--- home-assistant/run.sh
+++ home-assistant/run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+IMAGE=molobrakos/home-assistant:latest
+NAME=home-assistant
 CONFIG=$HOME/.docker-home-assistant/config
-
 mkdir -p $CONFIG
+
+docker stop $NAME
+docker rm $NAME
+docker pull $IMAGE
 
 docker run \
 -d \
---name home-assistant \
+--name $NAME \
 -p 8123:8123/tcp \
 -v /etc/localtime:/etc/localtime:ro \
 -v $CONFIG:/config \
-molobrakos/home-assistant:latest
+$IMAGE
+
+docker logs -f $NAME
+
