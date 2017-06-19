531f74b6a1954c78e2699c2757d57c798a3126e8
Erik Eriksson <molobrakos@users.noreply.github.com>
2017-06-19 19:24:34 +0200

    added motion

---
+++ motion/run.sh
@@ -0,0 +1,11 @@
+CONFIG=$HOME/.docker-motion
+mkdir -p $CONFIG
+
+docker rm -f motioneye ; docker run -it --name=motioneye \
+-p 8080:8080 \
+-p 8081:8081 \
+-p 8765:8765 \
+-v /etc/localtime:/etc/localtime:ro \
+-v $CONFIG:/config \
+--restart=always \
+molobrakos/motion
