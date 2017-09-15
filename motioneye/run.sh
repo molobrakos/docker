d598b85a44eab6721b5c8ca317c4651825977215
Erik <erik.eriksson@yahoo.com>
2017-09-15 20:42:07 +0200

    more ports

--- motioneye/run.sh
+++ motioneye/run.sh
@@ -11,6 +11,7 @@ docker run -d \
        -it --name=$CONTAINER \
        -p 8080:8080 \
        -p 8081:8081 \
+       -p 8082:8082 \
        -p 8765:8765 \
        -v /etc/localtime:/etc/localtime:ro \
        -v $CONFIG:/config \
