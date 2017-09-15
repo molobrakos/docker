1a54c6082c5dac60fa02c1faa4fd0ff71d7a7887
Erik <erik.eriksson@yahoo.com>
2017-09-15 20:36:11 +0200

    motion fix

--- motioneye/run.sh
+++ motioneye/run.sh
@@ -2,18 +2,18 @@ NAME=motioneye
 IMAGE=molobrakos/$NAME
 CONTAINER=$NAME
 
-MEDIA=/mnt/media
+MEDIA=/mnt/motion
 CONFIG=$HOME/.docker-$NAME
 mkdir -p $CONFIG
 
 docker rm -f $CONTAINER
-docker run -it --name=$CONTAINER \
--d \
--p 8080:8080 \
--p 8081:8081 \
--p 8765:8765 \
--v /etc/localtime:/etc/localtime:ro \
--v $CONFIG:/config \
--v $MEDIA:/media \
---restart=always \
+docker run -d \
+       -it --name=$CONTAINER \
+       -p 8080:8080 \
+       -p 8081:8081 \
+       -p 8765:8765 \
+       -v /etc/localtime:/etc/localtime:ro \
+       -v $CONFIG:/config \
+       -v $MEDIA:/media \
+       --restart=always \
 $IMAGE
