58e2fe60878ee98b81f63b89d69d5c0be63417d0
erik eriksson <error.errorsson@gmail.com>
2017-09-21 16:44:10 +0200

    added mosquitto

---
+++ mosquitto/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONTAINER=mqtt
+IMAGE=molobrakos/$CONTAINER:latest
+CONFIG=$HOME/.docker-$CONTAINER/config
+mkdir -p $CONFIG
+
+docker stop $CONTAINER
+docker rm $CONTAINER
+#docker pull $IMAGE
+
+docker run \
+-d \
+--name $CONTAINER \
+-p 1883:1883/tcp \
+-v /etc/localtime:/etc/localtime:ro \
+-v $CONFIG:/config \
+$IMAGE
