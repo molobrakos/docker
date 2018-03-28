a4ebdf76561c804261bb6c33fe7b63711ea17bd7
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-03-28 21:19:04 +0200

    Fix

--- lighttpd/run.sh
+++ lighttpd/run.sh
@@ -8,6 +8,7 @@ docker rm $CONTAINER
 docker pull $IMAGE
 
 docker run \
+       --detach \
        --name $CONTAINER \
        --restart=always \
        -p 80:80/tcp \
@@ -15,10 +16,10 @@ docker run \
        -v /etc/localtime:/etc/localtime:ro \
        -v /etc/lighttpd:/etc/lighttpd:ro \
        -v /var/run/lighttpd.pid:/var/run/lighttpd.pid \
-       -v /var/cache/lighttpd:/var/cache/lighttpd
+       -v /var/cache/lighttpd:/var/cache/lighttpd \
        -v /usr/share/lighttpd:/usr/share/lighttpd:ro \
        -v /var/www:/var/www:ro \
        -v /var/log/lighttpd:/var/log/lighttpd \
        -v $HOME/.docker-lighttpd/lighttpd.conf:/etc/lighttpd/lighttpd.conf:ro \
-       -v /etc/mime.types:/etc/mime.types:ro
+       -v /etc/mime.types:/etc/mime.types:ro \
         $IMAGE
