bd110ccc7bebb9839c5efa2bfa55123d93236494
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-03-28 18:47:29 +0200

    Added

--- lighttpd/run.sh
+++ lighttpd/run.sh
@@ -9,8 +9,16 @@ docker pull $IMAGE
 
 docker run \
        --name $CONTAINER \
+       --restart=always \
+       -p 80:80/tcp \
+       -p 443:443/tcp \
        -v /etc/localtime:/etc/localtime:ro \
        -v /etc/lighttpd:/etc/lighttpd:ro \
-       -v /var/www:/var/www:ro \
+       -v /var/run/lighttpd.pid:/var/run/lighttpd.pid \
+       -v /var/cache/lighttpd:/var/cache/lighttpd
        -v /usr/share/lighttpd:/usr/share/lighttpd:ro \
+       -v /var/www:/var/www:ro \
+       -v /var/log/lighttpd:/var/log/lighttpd \
+       -v $HOME/.docker-lighttpd/lighttpd.conf:/etc/lighttpd/lighttpd.conf:ro \
+       -v /etc/mime.types:/etc/mime.types:ro
         $IMAGE
