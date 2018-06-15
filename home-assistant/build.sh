a1bb089b453c5fb728c629e0d78194cab235c208
Erik Eriksson <molobrakos@users.noreply.github.com>
2018-06-15 09:41:44 +0200

    In short, switch to debian slim base image

--- home-assistant/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t molobrakos/home-assistant:base -f Dockerfile.base .
-docker build -t molobrakos/home-assistant .
-docker build -t molobrakos/home-assistant:dev -f Dockerfile.dev .
